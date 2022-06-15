06d562bd7bb1447738b882e42786cfaf05885a87
Ghania-Aitdriss <aitdgha77@hotmail.fr>
2022-06-15 22:17:40 +0200

    javascript fertig

---
+++ index.js
@@ -0,0 +1,28 @@
+const hourEl = document.getElementById('houre');
+const minutEl = document.getElementById('minutes');
+const secondEl = document.getElementById('seconds');
+const ampEl = document.getElementById('ampm');
+
+function updateClock() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = 'AM';
+    if (h > 12) {
+        h = h - 12;
+        ampm = 'PM';
+    }
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    s = s < 10 ? '0' + s : s;
+
+    hourEl.innerText = h;
+    minutEl.innerText = m;
+    secondEl.innerText = s;
+    ampEl.innerText = ampm;
+    setTimeout(() => {
+        updateClock();
+    }, 1000);
+}
+
+updateClock();
